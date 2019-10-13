269607a2cf2aba8232cd6e92353ee3ebd0973297
Airat <zalyaliev.evermore@gmail.com>
2019-10-13 23:02:41 +0300

    My Django app

---
+++ bboard/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import bbs
+
+from .views import BbDetailView
+
+
+urlpatterns = [
+    path('bbs/<int:pk>/', BbDetailView.as_view()),
+    path('bbs/', bbs),
+
+]
---
+++ bboard/main/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from .views import index
+from .views import other_page, profile
+from .views import BBLoginView, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView, RegisterUserView, RegisterDoneView, user_activate, DeleteUserView
+from .views  import by_rubric, profile_bb_detail, profile_bb_add, profile_bb_change, profile_bb_delete
+
+from .views import detail
+
+
+app_name='main'
+urlpatterns = [
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
+    path('accounts/register/activate/<str:sign>/', user_activate, name = 'register_activate'),
+    path('acconts/register/done', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
+    path('accoutns/password/change_password/', BBPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('profile/bb/add', profile_bb_add, name='profile_bb_add'),
+    path('account/profile/change/<int:pk>', profile_bb_change, name='profile_bb_change'),
+    path('account/profile/delete/<int:pk>', profile_bb_delete, name='profile_bb_delete'),    
+    path('account/profile/<int:pk>', profile_bb_detail, name='profile_bb_detail'),
+    path('accounts/profile/', profile, name='profile'),
+    path ('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
+    path('<int:pk>/', by_rubric, name='by_rubric'),
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
+]
